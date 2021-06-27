10d59973421df6f0c80b861e2769f98e4faef985
Jegor Jegorov <dev@jegorov.eu>
2021-06-27 15:24:39 +0300

    - added download btn

--- src/jegorov/components/pages/Resume.js
+++ src/jegorov/components/pages/Resume.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, useRef} from "react";
 import {Avatar, Box, CardMedia, Container, Divider, Grid, Typography} from "@material-ui/core";
 
 
@@ -22,34 +22,39 @@ import briefDescMd from "../../../resources/text/brief_description.md";
 import skills from "../../../resources/text/skills.md";
 
 
-
-
 import History from "../elements/History";
 import ReactMarkdown from "react-markdown";
+import { PDFExport, savePDF } from '@progress/kendo-react-pdf';
+
 
 
 class Resume extends Component {
 
-    componentDidMount () {
+    componentDidMount() {
         fetch(briefDescMd)
             .then(r => r.text())
             .then(data => {
-                this.setState({desc:data})
+                this.setState({desc: data})
             });
         fetch(skills)
             .then(r => r.text())
             .then(data => {
-                this.setState({skills:data})
+                this.setState({skills: data})
             });
     }
 
     constructor() {
         super();
         this.state = {}
-
+        this.pdfExportComponent = React.createRef();
     }
+
     openResumePage = () => {
         history.push("/")
+    }
+
+    handleExportWithComponent  = (event) => {
+        this.pdfExportComponent.current.save();
     }
 
     render() {
@@ -58,68 +63,74 @@ class Resume extends Component {
 
 
                 <Box className={"main-resume-container"}>
-                    <Box className={"menu-main-btn"}  onClick={() => this.openResumePage()}>Home</Box>
-                    {/*<Box className={"menu-main-btn"} onClick={() => this.openResumePage()}>Download</Box>*/}
+                    <Box className={"menu-main-btn"} onClick={() => this.openResumePage()}>Home</Box>
+                    <Box className={"menu-main-btn"} onClick={() => this.handleExportWithComponent()}>Download</Box>
 
-                    <Box className={"header-box"}>
-                        <Box  className={"brief-description-block "} style={{zIndex: "4"}}>
-                            <Box className={"h1"} >
-                            Jegor Jegorov
+                    <PDFExport id={"resume_container"} margin={5}  fileName={"resume_jegorov.pdf"} ref={this.pdfExportComponent} paperSize={"A3"}>
+                        <Box className={"header-box"}>
+                            <Box className={"brief-description-block "} style={{zIndex: "4"}}>
+                                <Box className={"h1"}>
+                                    Jegor Jegorov
+                                </Box>
+                                <br/>
+                                <Box className={"h2"}>
+                                    Software Engineer
+                                </Box>
+                                <br/>
+                                <Box className={"h3"}>
+                                    <ReactMarkdown>{this.state.desc && this.state.desc}</ReactMarkdown>
+                                </Box>
                             </Box>
-                            <br/>
-                            <Box className={"h2"} >
-                            Software Engineer
-                            </Box>
-                            <br/>
-                            <Box className={"h3"}>
-                                <ReactMarkdown>{this.state.desc && this.state.desc }</ReactMarkdown>
-                            </Box>
-                        </Box>
-                        <Box className={"inside-main-box"} style={{zIndex: "5"}}>
-                            {/*<Box className={"image-box"}>*/}
+                            <Box className={"inside-main-box"} style={{zIndex: "5"}}>
+                                {/*<Box className={"image-box"}>*/}
                                 <CardMedia className={"image"} image={photoImgUrl}/>
-                            {/*</Box>*/}
-                        </Box>
-                    </Box>
-                    <br/>
-                    <Box style={{height: "100%", position: "relative"}}>
-                        <Box className={"title-box"}>
-                            Work Experience
-                        </Box>
-                        <History imgSrc={hrImgUrl} name={"HireRight"} title={"Software Engineer"} location={"Tallinn, Estonia"}
-                                 years={"March, 2021 - Present"} webUrl={"hireright.com"} desc={hrDescMd}/>
-                        <History imgSrc={sberImgUrl} name={"Sberbank Technology"} title={"Software Engineer"}  location={"Moscow, Russia"}
-                                 years={"July, 2018 - February, 2021"} webUrl={"sberbank.com"} desc={sberDescMd}/>
-                        <History imgSrc={gridnineImgUrl} name={"Gridnine Systems"} title={"Software Engineer"}  location={"Moscow, Russia"}
-                                 years={"January, 2018 - July, 2018"} webUrl={"gridnine.com"} desc={gridnineDescMd}/>
-
-                        <Box className={"title-box"}>
-                            Education
-                        </Box>
-                        <History imgSrc={bonchImgUrl}
-                                 name={"The Bonch-Bruevich Saint Petersburg State University of Telecommunications"}
-                                 title={"Bachelor's degree,Information Technology"}
-                                 location={"Saint-Petersburg, Russia"} years={"2013 - 2017"}
-                                 webUrl={"sut.ru/eng"}/>
-                        <Box className={"title-box"}>
-                            Additional Certificates
-                        </Box>
-                        <History imgSrc={bonchImgUrl}
-                                 name={"The Bonch-Bruevich Saint Petersburg State University of Telecommunications"}
-                                 title={"Training: Translation And Interpretation SPBSUT (Eng-Rus)"}
-                                         location={"Saint-Petersburg, Russia"} years={"2014-2017"}/>
-                        <History imgSrc={redhatImgUrl}
-                                 name={"RedHat"}
-                                 title={"Introduction to Containers, Kubernetes, and Red Hat OpenShift (DO180)"}
-                                         years={"2019"}/>
-                        <Box className={"title-box"}>
-                            Skills
-                        </Box>
-                        <Box className={"skills-box"}>
-                            <ReactMarkdown>{this.state.skills && this.state.skills }</ReactMarkdown>
+                                {/*</Box>*/}
+                            </Box>
                         </Box>
                         <br/>
-                    </Box>
+                        <Box style={{height: "100%", position: "relative"}}>
+                            <Box className={"title-box"}>
+                                Work Experience
+                            </Box>
+                            <History imgSrc={hrImgUrl} name={"HireRight"} title={"Software Engineer"}
+                                     location={"Tallinn, Estonia"}
+                                     years={"March, 2021 - Present"} webUrl={"hireright.com"} desc={hrDescMd}/>
+                            <History imgSrc={sberImgUrl} name={"Sberbank Technology"} title={"Software Engineer"}
+                                     location={"Moscow, Russia"}
+                                     years={"July, 2018 - February, 2021"} webUrl={"sberbank.com"} desc={sberDescMd}/>
+                            <History imgSrc={gridnineImgUrl} name={"Gridnine Systems"} title={"Software Engineer"}
+                                     location={"Moscow, Russia"}
+                                     years={"January, 2018 - July, 2018"} webUrl={"gridnine.com"}
+                                     desc={gridnineDescMd}/>
+
+                            <Box className={"title-box"}>
+                                Education
+                            </Box>
+                            <History imgSrc={bonchImgUrl}
+                                     name={"The Bonch-Bruevich Saint Petersburg State University of Telecommunications"}
+                                     title={"Bachelor's degree,Information Technology"}
+                                     location={"Saint-Petersburg, Russia"} years={"2013 - 2017"}
+                                     webUrl={"sut.ru/eng"}/>
+                            <Box className={"title-box"}>
+                                Additional Certificates
+                            </Box>
+                            <History imgSrc={bonchImgUrl}
+                                     name={"The Bonch-Bruevich Saint Petersburg State University of Telecommunications"}
+                                     title={"Training: Translation And Interpretation SPBSUT (Eng-Rus)"}
+                                     location={"Saint-Petersburg, Russia"} years={"2014-2017"}/>
+                            <History imgSrc={redhatImgUrl}
+                                     name={"RedHat"}
+                                     title={"Introduction to Containers, Kubernetes, and Red Hat OpenShift (DO180)"}
+                                     years={"2019"}/>
+                            <Box className={"title-box"}>
+                                Skills
+                            </Box>
+                            <Box className={"skills-box"}>
+                                <ReactMarkdown>{this.state.skills && this.state.skills}</ReactMarkdown>
+                            </Box>
+                            <br/>
+                        </Box>
+                    </PDFExport>
                 </Box>
             </Box>
         )
